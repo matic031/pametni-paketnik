a255b1b040c018cc279bfd690d823ef513345b99
Matic Majerič <majericmatic@gmail.com>
2025-06-02 19:51:48 +0200

    porpavek porta, ki ga uporabimo za backend

--- PJ/app/src/main/java/com/example/pametnipaketnik/network/RetrofitInstance.kt
+++ PJ/app/src/main/java/com/example/pametnipaketnik/network/RetrofitInstance.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    private const val BASE_URL = "http://10.0.2.2:3001/" // Prilagoid
+    private const val BASE_URL = "http://10.0.2.2:3000/" // Prilagoid
 
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY // Prikazuje celotno telo zahteve/odgovora
